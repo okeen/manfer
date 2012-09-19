0e489f1ace8ad2e188ad294bb7e36154327c25e1
paula <caminopaula@gmail.com>
2012-09-19 21:40:50 +0200

    Añadidos seguros á pantalla de servicios

--- app/helpers/company_helper.rb
+++ app/helpers/company_helper.rb
@@ -2,4 +2,8 @@ module CompanyHelper
   def comunity_services_traductor(service)
     return t(".comunity."+service)
   end
+
+  def insurances_services_traductor(service)
+    return t(".insurance."+service)
+  end
 end
