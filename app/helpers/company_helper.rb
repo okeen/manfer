e51dc5d05ebafca07b3c9384f61c6d39773aab91
paula <caminopaula@gmail.com>
2012-09-20 19:00:42 +0200

    Añadidos os textos dos links na paxina de enlaces

--- app/helpers/company_helper.rb
+++ app/helpers/company_helper.rb
@@ -6,4 +6,12 @@ module CompanyHelper
   def insurances_services_traductor(service)
     return t(".insurance."+service)
   end
+
+  def links_interesting_traductor(link)
+    return t(".link_interesting."+link)
+  end
+
+  def links_city_traductor(link)
+    return t(".link_city."+link)
+  end
 end
