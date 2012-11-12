d89ba86fec6157100ab744efb2b329e2abf7ff15
Eneko Taberna <eneko.taberna@gmail.com>
2012-11-12 23:06:52 +0100

    Shit

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,7 @@
 class ContactsController < ApplicationController
   def create
     @contact = Contact.new params[:contact_mailer]
+    logger.info "ERRORS: #{@contact.errors.inspect}"
     if @contact.save
       flash[:info] = t("contact.created")
       redirect_to [:company, :home]
